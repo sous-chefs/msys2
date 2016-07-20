67c3e3d0b51ae11549665b0dab4991423b3d2a32
Brian Holtkamp <brholtkamp@gmail.com>
2016-07-19 22:16:22 -0500

    Adding in integration tests

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'MSYS2 installation' do
+  describe file('C:/msys64/') do
+    it { should be_directory }
+  end
+
+  describe file('C:/msys64/usr/bin/bash.exe') do
+    it { should exist }
+  end
+
+  describe command('C:/msys64/usr/bin/bash.exe -l -c "git --version"') do
+    its(:stdout) { should match /git version [0-9.]+/ }
+  end
+end
