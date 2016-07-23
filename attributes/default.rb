f4a0a58de384dbf5be29e8365abb51029c24e592
Brian Holtkamp <brholtkamp@gmail.com>
2016-07-23 00:22:31 -0500

    Switching overrides to false by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['msys2']['install_dir'] = 'C:/msys64'
 default['msys2']['packages'] = []
 default['msys2']['default_env'] = :msys
 default['msys2']['verbose'] = true
-default['msys2']['override_package'] = true
-default['msys2']['override_execute'] = true
+default['msys2']['override_package'] = false
+default['msys2']['override_execute'] = false
