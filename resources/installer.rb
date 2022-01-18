1799970f4e7b070aa72f9de966082c84dd2fdc81
kitchen-porter <board@sous-chefs.org>
2022-01-18 00:11:20 +0000

    Cookstyle 7.30.1 Fixes
    Issues found and resolved with: resources/installer.rb
    
     - 14:5 refactor: Chef/RedundantCode/UseCreateIfMissing - Use the :create_if_missing action instead of not_if with a ::File.existFOO check. https://docs.chef.io/workstation/cookstyle/chef_redundantcode_usecreateifmissing
     - 18:5 refactor: Chef/RedundantCode/UseCreateIfMissing - Use the :create_if_missing action instead of not_if with a ::File.existFOO check. https://docs.chef.io/workstation/cookstyle/chef_redundantcode_usecreateifmissing
    
    Signed-off-by: kitchen-porter <board@sous-chefs.org>

--- resources/installer.rb
+++ resources/installer.rb
@@ -10,12 +10,11 @@ unified_mode true
 action :run do
   remote_file 'C:/msys2.exe' do
     source 'https://downloads.sourceforge.net/project/msys2/Base/x86_64/msys2-x86_64-20160205.exe'
-    action :create
-    not_if { ::File.exist?('C:/msys2.exe') }
+    action :create_if_missing
   end
 
   cookbook_file 'C:/msys2.js' do
-    not_if { ::File.exist?('C:/msys2.js') }
+    action :create_if_missing
   end
 
   # Make sure we turn off the override in order to run this with standard execute
